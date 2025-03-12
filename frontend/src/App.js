c49eae4bbe1017f9c3db682490aa224fc79d5f32
Oleksandr Koval <oleksejvicvo@gmail.com>
2025-03-12 17:52:22 +0200

    first stage

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+   return (
+      <div className="App">
+         <header className="app-header">
+            <h1>Book Library App</h1>
+         </header>
+      </div>
+   );
+}
+
+export default App;
